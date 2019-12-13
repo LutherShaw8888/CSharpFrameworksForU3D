84bc711b95c5bd941df0610c9033891f8c0bf7c3
肖老师 <x_l_x1987@126.com>
2019-12-13 12:09:23 +0800

    新增项目

---
+++ 01_单例基类/MonoSingleton.cs
@@ -0,0 +1,23 @@
+﻿/**********************************************************************
+*	Copyright (C) 2019-2020
+* 	File Name:	MonoSingleton.cs
+*	Author:	Luther Shaw
+**********************************************************************/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LUTHER{
+public class MonoSingleton : MonoBehaviour {
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}
+}
+}
