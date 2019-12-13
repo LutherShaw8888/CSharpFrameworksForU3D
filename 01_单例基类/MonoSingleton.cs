d8fcbd89d9d7f56b5f3c125b4d8e62abcd24411b
肖老师 <x_l_x1987@126.com>
2019-12-13 13:52:57 +0800

    为单例添加必要的注释

--- 01_单例基类/MonoSingleton.cs
+++ 01_单例基类/MonoSingleton.cs
@@ -11,12 +11,16 @@ using UnityEngine.UI;
 
 public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T> {
 	private static T instance;
+	/// <summary>
+	/// 可在Unity3D生命周期任何一处方法中调用，包括Awake方法。
+	/// </summary>
+	/// <value></value>
 	public static T MonoInstance {
 		get {
 			if (instance == null) {
 				instance = FindObjectOfType<T> ();
 				if (instance == null) {
-					new GameObject ("MonoSingleton_" + typeof (T).Name).AddComponent<T> ();
+					new GameObject ("MonoSingleton_" + typeof (T).Name).AddComponent<T> ();//找不到物体时，实例化一个物体，并挂载单例脚本。
 				}
 			} else {
 				instance.Init ();
@@ -24,9 +28,15 @@ public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T> {
 			return instance;
 		}
 	}
+	/// <summary>
+	/// 只能在Awake方法之后调用
+	/// </summary>
 	void Awake () {
 		instance = this as T;
 		instance.Init ();
 	}
+	/// <summary>
+	/// 基类提供一个初始化方法
+	/// </summary>
 	public virtual void Init () { }
 }
