3546479246748ec2a8078e50c77ac3974ff38dbd
肖老师 <x_l_x1987@126.com>
2019-12-13 23:15:48 +0800

    新增Sphere延时自转脚本

---
+++ 03_Sphere延时自转/SphereRotateDelay.cs
@@ -0,0 +1,36 @@
+﻿/**********************************************************************
+ *	Copyright (C) 2019-2020
+ * 	File Name:	SphereRotateDelay.cs
+ *	Author:	Luther Shaw
+ **********************************************************************/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using wvr;
+
+public class SphereRotateDelay : MonoBehaviour {
+	[Range (10, 100)]
+	public int rotateSpeed = 10;
+	[Range (1, 5)]
+	public int delayTime = 3;
+	private float timer = 0f;
+	private Vector3 headRotate;
+	void Start () {
+		headRotate = WaveVR_DevicePoseTracker.HeadRotationValue;
+	}
+	void Update () {
+		SphereRotate ();
+	}
+	private void SphereRotate () {
+		if (WaveVR_DevicePoseTracker.HeadRotationValue == headRotate) {
+			timer += Time.deltaTime;
+			if (timer >= delayTime) {
+				transform.Rotate (Vector3.up * Time.deltaTime * -rotateSpeed, Space.Self);
+			}
+		} else {
+			timer = 0f;
+			headRotate = WaveVR_DevicePoseTracker.HeadRotationValue;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
  * @Author: Luther
  * @Date: 2019-12-13 12:32:13
  * @LastEditors: Luther
- * @LastEditTime: 2019-12-13 14:41:47
+ * @LastEditTime: 2019-12-13 23:15:07
  -->
 # CSharpFrameworksForU3D
 >Unity3D比较常用的C#框架（主要看脚本）
@@ -18,6 +18,7 @@
 #### [3.EventCenter](/02_解耦合/CentralSystem/EventCenter.cs)
 
 ### 三、Sphere延时自转
+#### [1.SphereRotateDelay](/03_Sphere延时自转/SphereRotateDelay.cs) `*注：本框架基于WaveSDK-3.0.2。`
 
 ### 四、VRUI框架
 
